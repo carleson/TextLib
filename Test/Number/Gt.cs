ab0386e11821bfcbda8a834fdf3956955bbae73c
Johan Carleson <johan.carleson@gmail.com>
2013-06-26 11:02:48 +0200

    Removed packages from ignore-file

---
+++ Test/Number/Gt.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: a491259
+ * Date: 2013-06-26
+ * Time: 09:23
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Test.Number
+{
+	/// <summary>
+	/// Description of Gt.
+	/// </summary>
+	public class Gt
+	{
+		public Gt()
+		{
+		}
+	}
+}
---
+++ Test/TestUtil.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: a491259
+ * Date: 2013-06-26
+ * Time: 09:27
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Test
+{
+	/// <summary>
+	/// Description of TestUtil.
+	/// </summary>
+	public class TestUtil
+	{
+		public TestUtil()
+		{
+		}
+	}
+}
